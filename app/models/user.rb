70e93749ef696670e1bdfb13ae48439a1483eb23
nakatayudai <102974156+nakatayudai@users.noreply.github.com>
2022-07-21 10:06:37 +0000

    before_sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  
+  has_many :book, dependent: :destroy
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  
+  resources :book, only: [:new, :index, :show, :edit, :destroy]
+  
+  
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
